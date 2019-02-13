0d9bddf18596e2aed1c9239cf3077b48687caead
Jesse Brunner <jesse.brunner@wsu.edu>
2019-02-13 17:06:36 -0500

    Changed up figure to show proportion of tests positive...more complicated than anticipated!

--- XIC_Analysis.R
+++ XIC_Analysis.R
@@ -28,9 +28,9 @@ Bsal <- subset(Bsal, select=c(1,4,5,6,7), subset=Target=="Bsal")
 colnames(Bsal) <- c("Well", "Target", "Sample", "Starting_Quantity", "Cq")
 Bsal <- Bsal[!grepl(pattern="gBlock", x=Bsal$Sample), ]
 Bsal <- Bsal[!grepl(pattern="NEG", x=Bsal$Sample, ignore.case=T), ]
-Bsal.agg <- aggregate(x=Bsal$Starting_Quantity, 
-                      by=list(Sample=Bsal$Sample), 
-                      FUN=mean, 
+Bsal.agg <- aggregate(x=Bsal$Starting_Quantity,
+                      by=list(Sample=Bsal$Sample),
+                      FUN=mean,
                       na.rm=T)
 colnames(Bsal.agg)[2] <- "Estimated_Copies"
 # Manually remove 1st run when samples are rerun for concensus
@@ -51,8 +51,8 @@ z.power <- c(1, 1.5, 2, 2.5, 3)
 prob <- c(0.6, 0.85, 0.95, 1, 0.95)
 
 plot(x=z.power, y=prob,
-     xlim=c(-0.25, 7.25), 
-     ylim=c(0, 1), 
+     xlim=c(-0.25, 7.25),
+     ylim=c(0, 1),
      xlab="Number of Zoospores Processed (log10 scale)",
      ylab="Detection Probability")
 
@@ -64,17 +64,34 @@ composite.powers <- c(final$Zoospore_Power, f.final$power.z)
 LOD <- data.frame(Pos=composite.positives, Z=as.numeric(composite.powers))
 LOD <- LOD[which(LOD$Z!="control"), ]
 
-library(ggplot2)
+# Changed the graph so it shows proportion of samples positive
+library(tidyverse)
 
-g <- ggplot(data=LOD, aes(x=Z, y=Pos)) +
-     stat_sum() +
-     xlab("Number of Zoospores Filtered (log10)") + 
-     ylab("Detection Probability") +
-     geom_smooth(method=glm, method.args=list(family=binomial(link="probit"))) +
-     scale_size_area()
-g
+# First group and calculate proportion and number of samples
+LOD %>%
+  group_by(Z) %>%
+  summarise(Proportion = mean(Pos),
+            N = n()) %>%
+  ggplot(data=., aes(x=Z, y=Proportion)) + # feed this grouped data into ggplot
+  geom_smooth(data=LOD, aes(x=Z, y=Pos), # unfortunately, need to orginal ungrouped
+              inherit.aes = FALSE,       # data to get regression to work
+              method=glm, method.args=list(family=binomial(link="probit"))) +
+  geom_point(aes(size = N)) + # add in the points
+  geom_hline(yintercept=0.95) + # add a horizontal line
+  geom_segment(aes(x=log10(199), xend=log10(199), y=0.95, yend=0)) + # extend vertical segment at LOD
+  scale_size_area(breaks = c(5,20,25)) +
+  xlab("Number of Zoospores Filtered (log10)") +
+  ylab("Detection Probability")
 
-LODProbit <- glm(Pos ~ Z, 
+# Previous version
+ggplot(data=LOD, aes(x=Z, y=Pos)) +
+  stat_sum() +
+  xlab("Number of Zoospores Filtered (log10)") +
+  ylab("Detection Probability") +
+  geom_smooth(method=glm, method.args=list(family=binomial(link="probit"))) +
+  scale_size_area()
+
+LODProbit <- glm(Pos ~ Z,
                 data=LOD,
                 family=binomial(link="probit"))
 x.index <- seq(0, 3, length.out=5000)
